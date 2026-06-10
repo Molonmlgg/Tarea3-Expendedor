65a132033d0c6058694e9ea3d2615ef0e68eb6f7
crist <araya.cama@gmail.com>
2026-06-09 22:14:04 -0400

    feat: pequeños ajustes de documentacion a las clases Bebida y Dulce.

--- src/main/java/modelo/Bebida.java
+++ src/main/java/modelo/Bebida.java
@@ -1,7 +1,15 @@
 package modelo;
 
+/**
+ * Clase abstracta que representa a todas las bebidas disponibles en la máquina.
+ * Hereda de Producto y sirve como clasificación general para CocaCola, Sprite y Fanta.
+ */
 public abstract class Bebida extends Producto{
 
+    /**
+     * Constructor base para las bebidas.
+     * @param tipo El tipo específico de bebida (enum TipoProducto) para definir su precio.
+     */
     public Bebida(TipoProducto tipo){
             super(tipo);
     }
--- src/main/java/modelo/Dulce.java
+++ src/main/java/modelo/Dulce.java
@@ -1,6 +1,15 @@
 package modelo;
 
+/**
+ * Clase abstracta que representa a todos los dulces o snacks disponibles en la máquina.
+ * Hereda de Producto y sirve como clasificación general para Super8 y Snickers.
+ */
 public abstract class Dulce extends Producto {
+
+    /**
+     * Constructor base para los dulces.
+     * @param tipo El tipo específico de dulce (enum TipoProducto) para definir su precio.
+     */
     public Dulce(TipoProducto tipo){
         super(tipo);
     }
